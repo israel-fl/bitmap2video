7557c7608eefa5b96b85d0ce742edd8e3d742a6f
Israel Flores <iflore04@gmail.com>
2020-06-28 15:26:48 -0400

    Initial Kotlin refactoring

--- README.md
+++ README.md
@@ -1,12 +1,18 @@
 # bitmap2video
+![](bitmap2video.gif)
+
 Generate video from Bitmaps or a Canvas in Android.
 
 Create mp4 video from Bitmaps or anything you can draw to a hardware accelerated Canvas.  Pure, simple Android MediaCodec implementation.  Requires no third party libs or NDK.
 
 Currently supports the MP4 container and both AVC/H264 and HEVC/H265.  Easily extensable to other supported formats.  
 
-Run the sample app or check out [CreateRunnable](app/src/main/java/com/homesoft/bitmap2video/CreateRunnable.java) for an example.
+Run the sample app or check out
+[CreateRunnable](app/src/main/java/com/homesoft/bitmap2video/CreateRunnable.java)
+and [MainActivity](app/src/main/java/com/homesoft/bitmap2video/MainActivity.java)
+for an example.
 
+# Dependencies
 Add it in your root build.gradle at the end of repositories:
 
     allprojects {
@@ -21,3 +27,92 @@ Add it in your root build.gradle at the end of repositories:
     dependencies {
       implementation 'com.github.dburckh:bitmap2video:1.0.0'
     }
+
+
+# Initialize library
+Create an `EncoderConfig` object with your specific requirements.
+
+```java
+final EncoderConfig encoderConfig;
+encoderConfig = new AvcEncoderConfig(FPS, BITRATE_DEFAULT);
+// or
+encoderConfig = new HevcEncoderConfig(FPS, BITRATE_DEFAULT);
+
+videoFile = getVideoFile(MainActivity.this, "test.mp4");
+encoderConfig.setPath(videoFile.getAbsolutePath());
+encoderConfig.setFramesPerImage(FRAMES_PER_IMAGE);
+encoderConfig.setHeight(DEFAULT_HEIGHT);
+encoderConfig.setWidth(DEFAULT_WIDTH);
+```
+
+Create an instance of the `FrameEncoder` and pass in your
+`EncoderConfig`
+
+```java
+final FrameEncoder frameEncoder = new FrameEncoder(context, encoderConfig);
+frameEncoder.start();
+```
+
+## Encode
+
+```java
+final Bitmap bitmap = BitmapFactory.decodeStream(resources.openRawResource(R.drawable.image1));
+frameEncoder.createFrame(bitmap);
+
+frameEncoder.releaseVideoEncoder();
+
+frameEncoder.releaseMuxer();
+```
+
+## Add audio 
+Set an `AssetFileDescriptor` for your audio track on the
+`EncoderConfig`
+```java
+encoderConfig.setAudioTrackFileDescriptor(getFileDescriptor(context, R.raw.sound_file));
+```
+
+#### Mux in the audio frames
+Muxing of the audio frames needs to happen after you release the
+`MediaCodec`
+
+```java
+frameEncoder.releaseVideoEncoder();
+
+// Mux in the audio after we release the video encoder
+frameEncoder.muxAudioFrames();
+
+frameEncoder.releaseMuxer();
+```
+
+### Convenience utility functions
+We provide a few functions to simplify a couple of tasks. These can be
+found as static methods under `FileUtils`
+
+##### Get an `AssetFileDescriptor` from a raw resource
+`getFileDescriptor(Context context, int R.raw.sound_file)`
+
+##### Get a `File` object for your video
+`getVideoFile(final Context context, final String fileName)`
+
+##### Export the created file to other applications
+`shareVideo(Context context, File file, String mimeType)`
+
+##### Note
+These utility functions use the library's `ContentProvider` with
+declared authorities and specified paths.
+
+#### Declare your own ContentProvider
+You can change which provider is being used and specify your paths if
+you register your own against the manifest and create your own
+resources. You must then pass in the appropriate paths to the functions.
+See the library's
+[AndroidManifest](app/src/main/java/com/homesoft/bitmap2video/library/src/main/AndroidManifest.xml)
+for an example.
+
+```java
+getVideoFile(Context context, String fileDir, String fileName)
+```
+and
+```java
+shareVideo(Context context, File file, String mimeType, String fileAuthority)
+```
---
+++ app/src/main/java/com/homesoft/bitmap2video/MainActivity.kt
@@ -0,0 +1,156 @@
+package com.homesoft.bitmap2video
+
+import android.Manifest
+import android.content.pm.PackageManager
+import android.media.MediaFormat
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.homesoft.bitmap2video.FileUtils.getVideoFile
+import com.homesoft.bitmap2video.FileUtils.shareVideo
+import com.homesoft.encoder.*
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import java.io.File
+
+/*
+ * Copyright (C) 2019 Homesoft, LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+class MainActivity : AppCompatActivity() {
+    companion object {
+        val TAG = MainActivity::class.java.simpleName
+        val imageArray: List<Int> = listOf(
+                R.raw.im1,
+                R.raw.im2,
+                R.raw.im3,
+                R.raw.im4
+        )
+    }
+
+    private val scope: CoroutineScope = CoroutineScope(Dispatchers.Default)
+    private var videoFile: File? = null
+    private var muxerConfig: MuxerConfig? = null
+    private var mimeType = MediaFormat.MIMETYPE_VIDEO_AVC
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Basic implementation
+        bt_make.setOnClickListener {
+            bt_make.isEnabled = false
+
+            videoFile = getVideoFile(this@MainActivity, "test.mp4")
+            videoFile?.run {
+                muxerConfig = MuxerConfig(this, 600, 600, mimeType, 3, 1F, 1500000)
+                val muxer = Muxer(this@MainActivity, muxerConfig!!)
+
+//                createVideo(muxer) // using callbacks
+                // or
+                createVideoAsync(muxer) // using co-routines
+            }
+        }
+
+        avc.isEnabled = isCodecSupported(mimeType)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M &&
+                checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 1234)
+        }
+
+        setListeners()
+    }
+
+    // TODO: Make DRY
+    private fun setListeners() {
+        avc.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) setCodec(MediaFormat.MIMETYPE_VIDEO_AVC)
+        }
+        hevc.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) setCodec(MediaFormat.MIMETYPE_VIDEO_HEVC)
+        }
+        bt_play.setOnClickListener {
+            videoFile?.run {
+                player.setVideoPath(this.absolutePath)
+                player.start()
+            }
+        }
+        bt_share.setOnClickListener {
+            Log.i(TAG, "Sharing video...")
+            muxerConfig?.run {
+                shareVideo(this@MainActivity, file, mimeType)
+            }
+        }
+    }
+
+    private fun setCodec(codec: String) {
+        if (isCodecSupported(codec)) {
+            mimeType = codec
+            muxerConfig?.mimeType = mimeType
+        } else {
+            Toast.makeText(this@MainActivity, "AVC Codec not supported", Toast.LENGTH_SHORT)
+                    .show()
+        }
+    }
+
+    // Callback-style approach
+    private fun createVideo(muxer: Muxer) {
+        muxer.setOnMuxingCompletedListener(object : MuxingCompletionListener {
+            override fun onVideoSuccessful(file: File) {
+                Log.d(TAG, "Video muxed - file path: ${file.absolutePath}")
+                onMuxerCompleted()
+            }
+
+            override fun onVideoError(error: Throwable) {
+                Log.e(TAG, "There was an error muxing the video")
+                onMuxerCompleted()
+            }
+        })
+
+        // Needs to happen on a background thread (long-running process)
+        Thread(Runnable {
+            muxer.mux(imageArray, R.raw.bensound_happyrock)
+        }).start()
+    }
+
+    private fun onMuxerCompleted() {
+        runOnUiThread {
+            bt_make.isEnabled = true
+            bt_play.isEnabled = true
+            bt_share.isEnabled = true
+        }
+    }
+
+    // Coroutine approach
+    private fun createVideoAsync(muxer: Muxer) {
+        scope.launch {
+            when (val result = muxer.muxAsync(imageArray, R.raw.bensound_happyrock)) {
+                is MuxingSuccess -> {
+                    Log.i(TAG, "Video muxed - file path: ${result.file.absolutePath}")
+                    onMuxerCompleted()
+                }
+                is MuxingError -> {
+                    Log.e(TAG, "There was an error muxing the video")
+                    bt_make.isEnabled = true
+                }
+            }
+        }
+    }
+}
